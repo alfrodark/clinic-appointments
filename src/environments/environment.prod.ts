9cec115f8a2114e54f194371b8bdac284acf47f8
Alfred Otchere <98951880+alfrodark@users.noreply.github.com>
2024-09-14 20:33:45 +0000

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,5 @@
 export const environment = {
   production: true, firebase:{
-    apiKey: "AIzaSyAQDlW5cYKnKa6fXXHoLwErHc6JzzJh_Yk",
-    authDomain: "alfrosoft-microsystems.firebaseapp.com",
-    databaseURL: "https://alfrosoft-microsystems-default-rtdb.firebaseio.com",
-    projectId: "alfrosoft-microsystems",
-    storageBucket: "alfrosoft-microsystems.appspot.com",
-    messagingSenderId: "212125906316",
-    appId: "1:212125906316:web:c325edfd9c0a18e6be12a9",
-    measurementId: "G-N8KRX9MPD9",
+    
   }
 };
